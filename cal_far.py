e7286eec6e05bc01bc272f1b006403439daef00b
kumkumhirve <kumkumhirve452@gmail.com>
2025-05-11 22:33:29 +0530

    Add files via upload

---
+++ cal_far.py
@@ -0,0 +1,10 @@
+
+#convert the celsius to ferhenheit
+'''Q5: Write a program to calculate temperature conversion from
+Celsius to Fahrenheit (F= (9/5) C+32)'''
+
+C = int(input("enter the calsius:"))
+
+
+F = (C*9/5 )+32
+print(F)
